2014e0e32f22bd6ad860be040a0a5ed6279f802e
Bill Raymond <bill.raymond@cambermast.com>
2022-09-01 19:54:34 +0000

    delete guest images

--- wf-00-guest-images-fi/guest-featured-images.sh
+++ wf-00-guest-images-fi/guest-featured-images.sh
@@ -24,4 +24,13 @@ magick convert ..{{detail.guest-photo}} `` ` ``# the guest image`` ` `` \\\\
 {% endfor %}
 {% endfor %}
 
-
+magick convert albert.jpg `# the guest image`\
+    -resize 250x250^ `# resize the image, but keep aspect ratio`\
+    -gravity Center \
+    -background transparent \
+    -extent 250x250 `# Crop to a perfect square`\
+    -bordercolor white `# Define the border color`\
+    -border 6 `# Create a border around the image`\
+    \( +clone -background black -shadow 60x5+6+6 \) \
+    +swap -background none -layers merge +repage \
+    face.png

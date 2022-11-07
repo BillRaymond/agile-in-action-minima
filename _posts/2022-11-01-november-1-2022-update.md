f70e5809212c2c6aa8e715faccd5d8647d911d45
Bill Raymond <bill.raymond@cambermast.com>
2022-11-07 23:39:25 +0000

    finalize code to create photos and featured images (no code to check if it exists though)

---
+++ _posts/2022-11-01-november-1-2022-update.md
@@ -0,0 +1,38 @@
+---
+layout: podcast-post
+categories: agile-in-action-podcast
+author: Bill Raymond
+date: 2022-11-01
+title: November 1, 2022 Update
+twitter-text: ''
+guest-details:
+- guest-name: Bill Raymond
+  guest-title: Host of the Agile in Action podcast
+  guest-photo: "/uploads/fall-scenery.png"
+  guest-links:
+  - guest-link-url: https://www.linkedin.com/in/williamraymond/
+    guest-link-label: Bill's personal LinkedIn
+  - guest-link-url: https://www.linkedin.com/company/agile-in-action-with-bill-raymond-podcast
+    guest-link-label: Agile in Action podcast link
+libsynUrl: 'https://directory.libsyn.com/episode/index/id/24862260'
+
+---
+## Transcript
+
+(transcripts are auto-generated, so please excuse the brevity)
+**Bill Raymond:** Hi, this is bill Raymond host of the agile and action podcast. Today is Tuesday, November 1st, 2022. Now we have a lot of content to share with you over the coming weeks and months. And that means we need to prepare So this week, we're taking a little break and we'll be back on Tuesday, November 8, with a brand new podcast. 
+
+**Bill Raymond:** Until then I have a favor to ask of you. 
+
+**Bill Raymond:** If you have a LinkedIn account, could you go to the Agile in Action with Bill Raymond Page and subscribe? There you will get the latest information and all of our podcasts, including agile news. If you are listening to this podcast right now, just scroll down to the show notes or the description, and you'll find a link there, or you can get that on the https://agileinaction.com website. 
+
+**Bill Raymond:** Thank you and see you next week!
+
+**Sponsor:** The future of work is, shall we say, stormy, and it looks like things are not changing anytime soon. So how do you weather these storms and stay competitive while continuing to innovate? Recent studies have shown that when you put psychological safety first, people will work at their highest IQ and be capable of analytical thinking and collaborative problem solving. Do you want to see these improvements in your organization? If so, I want to introduce you to Alla Weinberg, founder of Spoke and Wheel.
+
+Alla and her talented team will perform a 90-day psychological safety sprint that will assess your culture against three critical areas.
+
+**Sponsor:** I suggest you set up a free 20-minute call with Alla and the Spoke and Wheel team.
+
+**Sponsor:** You can access this free call by visiting www.spokeandwheel.co/assess. That's like the spokes on a bicycle wheel, www.spokeandwheel.co/assess. When you sign up for the free call, mention the Agile In Action Podcast to receive a 10% discount on your psychological safety assessment. 
+
--- wf-00-guest-images-fi/guest-featured-images.sh
+++ wf-00-guest-images-fi/guest-featured-images.sh
@@ -4,51 +4,50 @@ layout: shell
 #!/bin/bash
 # -e Exit immediately if a command exits with a non-zero status
 # -x Echo all the commands as they run, not just echos
-set -e -x
+set -e -x&#10;
 
-{% comment %} note: make sure to run chmod +x script-name in the final solution.sh {% endcomment %}
-{% comment %} create guest images for reuse on featured images {% endcomment %}
-{% comment %} Resize and crop the guest image so it is square, has a border, and has a shadow {% endcomment %}
-{% comment %} Make sure you have a root folder structure like this: /uploads/wf-guest-images-fi {% endcomment %}
+{%- comment -%} note: make sure to run chmod +x script-name in the final solution.sh {% endcomment %}
+{%- comment -%} create guest images for reuse on featured images {% endcomment %}
+{%- comment -%} Resize and crop the guest image so it is square, has a border, and has a shadow {% endcomment %}
+{%- comment -%} Make sure you have a root folder structure like this: /uploads/wf-guest-images-fi {% endcomment %}
 {%- assign posts = site.posts | where_exp: 'post', 'post.guest-details !=nil' -%} 
 
-{% for post in posts %}
-{% for detail in post.guest-details %}
-{% comment %}
+{%- for post in posts -%}
+{%- for detail in post.guest-details -%}
+{%- comment -%}
     Set the location of the source image
         1. pre-pend with two dots ".." 
            since so the shell goes a level above this folder
-{% endcomment %}
-{% assign photoSource = detail.guest-photo %}
+{%- endcomment -%}
+{%- assign photoSource = detail.guest-photo -%}
 
-{% comment %}
+{%- comment -%}
     Set the target location for the final file
         1. Get the filename of the source photo
         2. Remove the file extension
         3. Add the png extension
         4. Pre-pend with the location to store the image
-{% endcomment %} 
-{% assign photoTarget = detail.guest-photo 
+{%- endcomment -%} 
+{%- assign photoTarget = detail.guest-photo 
     | split: '/' | last 
     | split: '.' | first
     | append: '.png' 
     | prepend: '../uploads/wf-guest-images-fi/'
-%}
+-%}
 
-
-{% comment %} *** START guest image conversion process *** {% endcomment%}
-echo "Converting guest image: {{photoSource}}"
-magick convert ..{{photoSource}} `` ` ``# the guest image`` ` `` \\\\
-    -resize 250x250^ `` ` ``# resize the image, but keep aspect ratio`` ` `` \\\\
-    -gravity center \\\\
-    -background transparent \\\\
-    -extent 250x250 `` ` ``# Crop to a perfect square`` ` `` \\\\
-    -bordercolor white `` ` ``# Define the border color`` ` `` \\\\
-    -border 6 `` ` ``# Create a border around the image`` ` `` \\\\
-    \\\( +clone -background black -shadow 60x5+6+6 \\\) \\\\
-    +swap -background none -layers merge +repage `` ` ``# merge the layers`` ` `` \\\\
-    {{photoTarget}} `` ` ``# save the guest image`` ` ``
-echo "converted guest image to: {{photoTarget}}"
-{% endfor %}
-{% endfor %}
+{%- comment -%} *** START guest photo conversion process *** {%-endcomment-%}
+echo "* START Converting guest photo: {{photoSource}}"
+magick convert ..{{photoSource}} &#96;# load the guest's photo&#96;&#92;
+    -resize 250x250^ &#96;# load template background image&#96;&#92;
+    -gravity center &#96;# Set the center of gravity for the photo&#96;&#92;
+    -background transparent &#96;# Background should be transparent&#96;&#92;
+    -extent 250x250 &#96;# Crop the photo to a perfect square&#96;&#92;
+    -bordercolor white &#96;# Define a border for the photo&#96;&#92;
+    -border 6 &#96;# Create a border around the photo&#96;&#92;
+    \( +clone -background black -shadow 60x5+6+6 \) &#96;# Create a shadow under the photo&#96;&#92;
+    +swap -background none -layers merge +repage &#96;# Merge the photo and shadow layers&#96;&#92;
+    {{photoTarget}} &#96;# Save the guest photo&#96;&#92;&#10;
+echo "* FINISH converted guest photo to: {{photoTarget}}"
+{%- endfor -%}
+{%- endfor -%}
 
